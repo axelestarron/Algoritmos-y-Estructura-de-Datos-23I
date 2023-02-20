c38ec013934c91b4707068b35fdcebd8afc94bf5
axelestarron <125074340+axelestarron@users.noreply.github.com>
2023-02-20 16:23:24 +0000

    Adición de código que implementa string tokenizer

--- cadenasPerronas.java
+++ cadenasPerronas.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class cadenasPerronas {
+
+//Ejercicio de comparación de palindromos
     public static void comparacionPalindromos(){
         String palindromo=new String("anitalavalatina");
         int longitud=palindromo.length();
@@ -19,16 +22,15 @@ public class cadenasPerronas {
             System.out.println("No es un palindromo maifren");
         }
     }
+//Ejercicio de conteo de caracteres
     public static void conteoCaracteres(String cadenaEntrada){
         int contador=0;
         char caracterContado;
         Scanner input1=new Scanner(System.in);
         System.out.println("Introduzca el caracter que quiere contar: ");
         caracterContado=input1.next().charAt(0);
-
-
     }
-
+//Ejercicio de patrón de concatenación 
     public static void concatenacionPerrona(int n){
         String cadenota=new String();
         for (int i=1;i<=n;i++){
@@ -41,7 +43,21 @@ public class cadenasPerronas {
             System.out.println(cadenota);
         }
     }
+//Implementación de StringTokenizer
+    public static void divisorLexico(){
+        Scanner input1=new Scanner(System.in);
+        System.out.println("Escriba una oración");
+        String oracion=input1.nextLine();
+        System.out.println("Cadena a procesar: "+oracion);
+        //componentes lexicográficos
+        StringTokenizer st=new StringTokenizer(oracion);
+        while (st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+        input1.close();
+    }
+
     public static void main(String[] args){
-        concatenacionPerrona(5);        
+        divisorLexico();       
     }
 }
