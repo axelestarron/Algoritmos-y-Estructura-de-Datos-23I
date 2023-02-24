d414c0ec71de98ba720aaa5204e8b69d1da75c4e
axelestarron <125074340+axelestarron@users.noreply.github.com>
2023-02-24 17:20:38 +0000

    no lo vuelvas a cerrar

--- EjemploArreglo.java
+++ EjemploArreglo.java
@@ -2,112 +2,146 @@ import java.util.Scanner;
 
 public class EjemploArreglo {
     int miArreglo[];
-//métodos constructores
-    EjemploArreglo(){
-        Scanner entradaArreglo=new Scanner(System.in);
+
+    // métodos constructores
+    EjemploArreglo() {
+        Scanner entradaArreglo = new Scanner(System.in);
         System.out.print("Introduzca el tamaño de su arreglo: ");
-        int capacidad=entradaArreglo.nextInt();
-        miArreglo=new int[capacidad];
+        int capacidad = entradaArreglo.nextInt();
+        miArreglo = new int[capacidad];
 
-        System.out.println("\n"+"Introduzca los elementos del arreglo.");
-        for (int i=0;i<miArreglo.length;i++){
-            System.out.print("Elemento "+ (i+1)+": ");
-            miArreglo[i]=entradaArreglo.nextInt();
-        }
-        entradaArreglo.close();
-    }
-        
-    EjemploArreglo(int tamaño){
-        Scanner entradaArreglo=new Scanner(System.in);
-        miArreglo=new int[tamaño];
-            System.out.println("\n"+"Introduzca los elementos del arreglo.");
-
-        for (int i=0;i<miArreglo.length;i++){
-            System.out.print("Elemento "+ (i+1)+": ");
-            miArreglo[i]=entradaArreglo.nextInt();
+        System.out.println("\n" + "Introduzca los elementos del arreglo.");
+        for (int i = 0; i < miArreglo.length; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            miArreglo[i] = entradaArreglo.nextInt();
         }
         entradaArreglo.close();
     }
 
-    EjemploArreglo(int a[]){
-        this.miArreglo=a;
+    EjemploArreglo(int tamaño) {
+        Scanner entradaArreglo = new Scanner(System.in);
+        miArreglo = new int[tamaño];
+        System.out.println("\n" + "Introduzca los elementos del arreglo.");
+
+        for (int i = 0; i < miArreglo.length; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            miArreglo[i] = entradaArreglo.nextInt();
+        }
+        entradaArreglo.close();
     }
-//métodos para los arreglos
-    public int suma(){
-        int result=0;
-        for (int i=0;i<miArreglo.length;i++){
-            result=result+miArreglo[i];
+
+    EjemploArreglo(int a[]) {
+        this.miArreglo = a;
+    }
+
+    // métodos para los arreglos
+    public int suma() {
+        int result = 0;
+        for (int i = 0; i < miArreglo.length; i++) {
+            result = result + miArreglo[i];
         }
         return result;
     }
 
-    public int maximo(){
-        int result=miArreglo[0];
-        for (int i=1;i<miArreglo.length;i++){
-            if(miArreglo[i]>result){
-                result=miArreglo[i];
+    public double promedio() {
+        double prom = this.suma();
+        prom = prom / 2;
+        return prom;
+    }
+
+    public void insertSort() {
+        int min, temp;
+        for (int i = 0; i < (this.miArreglo.length - 1); i++) {
+            min = i;
+
+            for (int j = i + 1; j < this.miArreglo.length; j++) {
+                if (this.miArreglo[j] < this.miArreglo[min]) {
+                    min = j;
+                }
+            }
+
+            temp = this.miArreglo[min];
+            this.miArreglo[min] = this.miArreglo[i];
+            this.miArreglo[i] = temp;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = new String("{" + this.miArreglo[0]);
+        for (int i = 1; i < this.miArreglo.length; i++) {
+            result = result.concat("," + this.miArreglo[i]);
+        }
+        result = result.concat("}");
+        return result;
+    }
+
+    // maximo
+    public int maximo() {
+        int result = miArreglo[0];
+        for (int i = 1; i < miArreglo.length; i++) {
+            if (miArreglo[i] > result) {
+                result = miArreglo[i];
             }
         }
         return result;
     }
 
-    public int posMaximo(){
-        int posMax=0;
-        for (int i=1;i<miArreglo.length;i++){
-            if(miArreglo[i]>miArreglo[posMax]){
-                posMax=i;
+    public int posMaximo() {
+        int posMax = 0;
+        for (int i = 1; i < miArreglo.length; i++) {
+            if (miArreglo[i] > miArreglo[posMax]) {
+                posMax = i;
             }
         }
         return posMax;
     }
 
-    public int valorMedio(){
-        return miArreglo[ miArreglo.length /2];
-    }
-
-    public double promedio(){
-       double prom= this.suma();
-       prom=prom/2;
-       return prom;
-    }
-
-    public void insertSort(){
-        int min, temp;
-        for (int i=0;i<(this.miArreglo.length -1);i++){
-            min=i;
-            for (int j=i+1;j<this.miArreglo.length;j++){
-                if (this.miArreglo[j]<this.miArreglo[min]){
-                    min=j;
-                }
+    // minimo
+    public int minimo() {
+        int result = miArreglo[0];
+        for (int i = 1; i < miArreglo.length; i++) {
+            if (miArreglo[i] < result) {
+                result = miArreglo[i];
             }
-            temp=this.miArreglo[min];
-            this.miArreglo[min]=this.miArreglo[i];
-            this.miArreglo[i]=temp;
         }
-    }
-
-    @Override
-    public String toString(){
-        String result=new String("{"+ this.miArreglo[0]);
-        for (int i=1;i<this.miArreglo.length;i++){
-            result=result.concat(","+ this.miArreglo[i]);
-        }
-        result=result.concat("}");
         return result;
     }
 
-//el main 
-    public static void main(String[] arg){
-        int a[]={2,1,2,4,3};
-        EjemploArreglo mikeWazowski=new EjemploArreglo(a);
-        //System.out.println(abominacion.toString());
-        //System.out.println("La suma de los elementos del arreglo es: "+ abominacion.suma());
-        //System.out.println("El máximo de su arreglo es: "+ abominacion.maximo() + ", en el indice: " + abominacion.posMaximo());
-        //System.out.println("El valor medio es: " + abominacion.valorMedio() + ", en el indice: " + abominacion.miArreglo.length /2);
-        //System.out.println("El promedio de su arreglo es: "+ abominacion.promedio()); 
-        mikeWazowski.insertSort();
-        System.out.println("El arreglo ordenado es: "+mikeWazowski.toString());
+    public int posMinimo() {
+        int posMin = 0;
+        for (int i = 1; i < miArreglo.length; i++) {
+            if (miArreglo[i] < miArreglo[posMin]) {
+                posMin = i;
+            }
+        }
+        return posMin;
+    }
+
+    // valor medio
+    public int valorMedio() {
+        return miArreglo[miArreglo.length / 2];
+    }
+
+    public int posMedio() {
+        return miArreglo.length / 2;
+    }
+
+    // el main
+    public static void main(String[] arg) {
+        int a[] = { 2, 1, 2, 4, 1 };
+        EjemploArreglo mikeWazowski = new EjemploArreglo(a);
+        // System.out.println(abominacion.toString());
+        // System.out.println("La suma de los elementos del arreglo es: "+
+        // abominacion.suma());
+        // System.out.println("El máximo de su arreglo es: "+ abominacion.maximo() + ",
+        // en el indice: " + abominacion.posMaximo());
+        // System.out.println("El valor medio es: " + abominacion.valorMedio() + ", en
+        // el indice: " + abominacion.miArreglo.length /2);
+        // System.out.println("El promedio de su arreglo es: "+ abominacion.promedio());
+        // mikeWazowski.insertSort();
+        // System.out.println("El arreglo ordenado es: "+mikeWazowski.toString());
+        System.out.println(
+                "El mínimo de su arreglo es: " + mikeWazowski.minimo() + " en el índice: " + mikeWazowski.posMinimo());
     }
 }
- 
-
--- estrellitas.java
+++ estrellitas.java
@@ -1,61 +1,67 @@
 import java.util.Scanner;
-public class estrellitas {
-    public static void main(String[] args){
-        System.out.println("Este programa imprime estrellas en diferentes patrones.");
-        int patron,repetir;
-        Scanner input1=new Scanner(System.in); 
 
-        do{
+public class estrellitas {
+    public static void main(String[] args) {
+        System.out.println("Este programa imprime estrellas en diferentes patrones.");
+        int patron, repetir;
+        Scanner input1 = new Scanner(System.in);
+
+        do {
             System.out.println("Seleccione la esquina en la que quiere el patrón:");
-            System.out.println("1:Superior izquierda." +"\n"+ "2:Superior derecha."+"\n"+"3:Inferior izquierda."+"\n"+"4:Inferior derecha.");
-            patron=input1.nextInt();
+            System.out.println("1:Superior izquierda." + "\n" + "2:Superior derecha." + "\n" + "3:Inferior izquierda."
+                    + "\n" + "4:Inferior derecha.");
+            patron = input1.nextInt();
             imprimirPatron(patron);
             System.out.println("Si desea repetir el programa, presione 1");
-            repetir=input1.nextInt();
-        }while(repetir==1);
-        
+            repetir = input1.nextInt();
+        } while (repetir==1);
+
         System.out.println("Gracias por usar este programa :)");
         input1.close();
     }
 
-    public static void imprimirPatron(int a){
-        int c1=0,c2=0,condicion=0;
-        boolean checkEntrada=true;
+    public static void imprimirPatron(int a) {
+        int c1 = 0, c2 = 0, condicion = 0;
+        boolean checkEntrada = true;
 
-        switch(a){
-            case 1:a=1;
-                c1=1;
-                c2=1;
-                condicion=5;
-            break;
-            case 2:a=2;
-                c1=1;
-                c2=-1;
-                condicion=1;
-            break;
-            case 3:a=3;
-                c1=-1;
-                c2=1;
-                condicion=1;
-            break;
-            case 4:a=4;
-                c1=-1;
-                c2=-1;
-                condicion=-3;
-            break;
+        switch (a) {
+            case 1:
+                a = 1;
+                c1 = 1;
+                c2 = 1;
+                condicion = 5;
+                break;
+            case 2:
+                a = 2;
+                c1 = 1;
+                c2 = -1;
+                condicion = 1;
+                break;
+            case 3:
+                a = 3;
+                c1 = -1;
+                c2 = 1;
+                condicion = 1;
+                break;
+            case 4:
+                a = 4;
+                c1 = -1;
+                c2 = -1;
+                condicion = -3;
+                break;
             default:
                 System.out.println("Entrada erronea");
-                checkEntrada=false;
-            break;
+                checkEntrada = false;
+                break;
         }
 
-        if (checkEntrada==true){
-            for(int i=0;i<5;i++){
-                for(int j=0;j<5;j++){
-                    if((c1*i+c2*j)<condicion){
-                        System.out.print("*");
-                    }else{
-                        System.out.print("o");
+        if (checkEntrada == true) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if ((c1 * i + c2 * j) < condicion) {
+                        System.out.print("X");
+                    } else {
+                        System.out.print(" ");
                     }
                 }
                 System.out.print("\n");
