ae4b4ec4ba86439a8b7166ece9decc9f086904df
axelestarron <125074340+axelestarron@users.noreply.github.com>
2023-03-10 02:51:50 +0000

    Se terminaron los programas de TareaComplejos y UsuarioComplejos

--- Tareas/TareaComplejos.java
+++ Tareas/TareaComplejos.java
@@ -3,86 +3,114 @@ package Tareas;
 public class TareaComplejos {
     public double re;
     public double im;
-    //Empty constructor
-    public TareaComplejos(){
-        re=0;
-        im=0;
+
+    // Empty constructor
+    public TareaComplejos() {
+        re = 0;
+        im = 0;
     }
-    //Constructor with parameters
-    public TareaComplejos(double a, double b){
-        re=a;
-        im=b;
+
+    // Constructor with parameters
+    public TareaComplejos(double a, double b) {
+        re = a;
+        im = b;
     }
-    //Add method
-    TareaComplejos suma(TareaComplejos sumando){
-        TareaComplejos resultado=new TareaComplejos();
-        resultado.re=this.re+sumando.re;
-        resultado.im=this.im+sumando.im;
+
+    // Add method
+    TareaComplejos suma(TareaComplejos sumando) {
+        TareaComplejos resultado = new TareaComplejos();
+        resultado.re = this.re + sumando.re;
+        resultado.im = this.im + sumando.im;
         return resultado;
     }
-    //Subtract method
-    TareaComplejos resta(TareaComplejos restando){
-        TareaComplejos resultado=new TareaComplejos();
-        resultado.re=this.re-restando.re;
-        resultado.im=this.im-restando.im;
+
+    // Subtract method
+    TareaComplejos resta(TareaComplejos restando) {
+        TareaComplejos resultado = new TareaComplejos();
+        resultado.re = this.re - restando.re;
+        resultado.im = this.im - restando.im;
         return resultado;
     }
-    //Product method
-    TareaComplejos producto(TareaComplejos factor){
-        TareaComplejos resultado=new TareaComplejos();
-        resultado.re=(this.re*factor.re)-(this.im*factor.im);
-        resultado.im=(this.re*factor.im)+(this.im*factor.re);
+
+    // Product method
+    TareaComplejos producto(TareaComplejos factor) {
+        TareaComplejos resultado = new TareaComplejos();
+        resultado.re = (this.re * factor.re) - (this.im * factor.im);
+        resultado.im = (this.re * factor.im) + (this.im * factor.re);
         return resultado;
     }
-    //Squared magnitude method
-    double magnitud(){
-        return this.re*this.re + this.im*this.im;
+
+    // Squared magnitude method
+    double magnitud() {
+        return this.re * this.re + this.im * this.im;
     }
-    //complement methodo
-    TareaComplejos complemento(){
-        TareaComplejos resultado=new TareaComplejos();
-        resultado.re=this.re;
-        resultado.im=-this.im;
+
+    // complement methodo
+    TareaComplejos complemento() {
+        TareaComplejos resultado = new TareaComplejos();
+        resultado.re = this.re;
+        resultado.im = -this.im;
         return resultado;
     }
-    //Division method
-    TareaComplejos division(TareaComplejos divisor){
-        if(divisor.re==0 && divisor.im==0){
+
+    // Division method
+    TareaComplejos division(TareaComplejos divisor) {
+        if (divisor.re == 0 && divisor.im == 0) {
             System.out.println("Error, no se puede dividir un número entre 0");
             return null;
-        }else{
-            TareaComplejos resultado=new TareaComplejos();
-            resultado=this.producto(divisor.complemento());
-            resultado.re=resultado.re/divisor.magnitud();
-            resultado.im=resultado.im/divisor.magnitud();
+        } else {
+            TareaComplejos resultado = new TareaComplejos();
+            resultado = this.producto(divisor.complemento());
+            resultado.re = resultado.re / divisor.magnitud();
+            resultado.im = resultado.im / divisor.magnitud();
             return resultado;
         }
     }
-    //Utility methods
-    //toString
+
+    // Utility methods
+    // toString
     @Override
-    public String toString(){
-        return "("+this.re+")"+"+ ("+this.im+")i";
+    public String toString() {
+        return "(" + this.re + ")" + "+ (" + this.im + ")i";
     }
 
-    boolean equals(TareaComplejos comparado){
-        return this.re==comparado.re && this.im==comparado.im;
+    // equals
+    public boolean equals(TareaComplejos comparado) {
+        return this.re == comparado.re && this.im == comparado.im;
     }
 
-    int compareTo(TareaComplejos comparado){
-        if(this.equals(comparado)){
+    // magnitude comparer
+    public int compareTo(TareaComplejos comparado) {
+        if (this.equals(comparado)) {
             return 0;
-        }else if(this.magnitud()>comparado.magnitud()){
+        } else if (this.magnitud() > comparado.magnitud()) {
             return 1;
-        }else{
+        } else {
             return -1;
         }
     }
 
-    public static void main(String[]arg){
-        TareaComplejos a=new TareaComplejos(2,3);
-        TareaComplejos b=new TareaComplejos(3,4);
-        System.out.println(a.division(b).toString());
+    // copy
+    public TareaComplejos copyOfTareaComplejos() {
+        TareaComplejos copia = new TareaComplejos(this.re, this.im);
+        return copia;
+    }
 
+    // get methods
+    public double getReal() {
+        return this.re;
+    }
+
+    public double getImaginary() {
+        return this.im;
+    }
+
+    // set methods
+    public void setReal(double newReal) {
+        this.re = newReal;
+    }
+
+    public void setImaginary(double newImaginary) {
+        this.im = newImaginary;
     }
 }
--- Tareas/UsuarioComplejos.java
+++ Tareas/UsuarioComplejos.java
@@ -1,9 +1,11 @@
 package Tareas;
+
 import java.util.Scanner;
+
 public class UsuarioComplejos {
-    
-    public static TareaComplejos operaciones(int a, TareaComplejos X, TareaComplejos Y){
-        switch(a){
+
+    public static TareaComplejos operaciones(int a, TareaComplejos X, TareaComplejos Y) {
+        switch (a) {
             case 1:
                 return X.suma(Y);
             case 2:
@@ -18,38 +20,49 @@ public class UsuarioComplejos {
         }
     }
 
-    public static void main(String[] arg){
-        TareaComplejos complejo1=new TareaComplejos();
-        TareaComplejos complejo2=new TareaComplejos();
-        Scanner input1=new Scanner(System.in);
-        int opcionOperacion,opcionContinuar;
-        int opcionRepValores=0;
+    public static void main(String[] arg) {
+        TareaComplejos complejo1 = new TareaComplejos();
+        TareaComplejos complejo2 = complejo1.copyOfTareaComplejos();
+        Scanner input1 = new Scanner(System.in);
+        int opcionOperacion, opcionContinuar;
+        int opcionRepValores = 0;
+        double aux;
         System.out.println("\n Este programa realiza operaciones con dos números complejos.");
-        do{
-            System.out.println("Selecione una de las siguientes opciones: \n 1. Sumar. \n 2. Restar. \n 3. Multiplicar. \n 4. Dividir.");
-            opcionOperacion=input1.nextInt();
-            if (opcionRepValores!=1){
-                //Lectura del primer número complejo
+        do {
+            System.out.println(
+                    "Selecione una de las siguientes opciones: \n 1. Sumar. \n 2. Restar. \n 3. Multiplicar. \n 4. Dividir.");
+            opcionOperacion = input1.nextInt();
+            if (opcionRepValores != 1) {
+                // Lectura del primer número complejo
                 System.out.println("Introduzca la parte real e imaginaria del primer número:");
-                System.out.print("Re: "); complejo1.re=input1.nextDouble();
-                System.out.print("Im: "); complejo1.im=input1.nextDouble();
-                //Lectura del segundo número complejo
+                System.out.print("Re: ");
+                aux = input1.nextDouble();
+                complejo1.setReal(aux);
+                System.out.print("Im: ");
+                aux = input1.nextDouble();
+                complejo1.setImaginary(aux);
+                // Lectura del segundo número complejo
                 System.out.println("Introduzca la parte real e imaginaria del segundo número: ");
-                System.out.print("Re: "); complejo2.re=input1.nextDouble();
-                System.out.print("Im: "); complejo2.im=input1.nextDouble();
+                System.out.print("Re: ");
+                aux = input1.nextDouble();
+                complejo2.setReal(aux);
+                System.out.print("Im: ");
+                aux = input1.nextDouble();
+                complejo2.setImaginary(aux);
             }
 
-            System.out.println("El resultado de la operación es: " + operaciones(opcionOperacion, complejo1,complejo2).toString());
+            System.out.println("El resultado de la operación es: "
+                    + operaciones(opcionOperacion, complejo1, complejo2).toString());
             System.out.println("Si desea utilizar el programa nuevamente, presione 1; sino, presione otra tecla.");
-            opcionContinuar=input1.nextInt();
-            if(opcionContinuar==1){
-                System.out.println("Si desea utilizar los mismos numeros complejos, presione 1; sino, presione otra tecla.");
-                opcionRepValores=input1.nextInt();
+            opcionContinuar = input1.nextInt();
+            if (opcionContinuar == 1) {
+                System.out.println(
+                        "Si desea utilizar los mismos numeros complejos, presione 1; sino, presione otra tecla.");
+                opcionRepValores = input1.nextInt();
             }
-        }while(opcionContinuar==1);
-        
+        } while (opcionContinuar == 1);
+
         System.out.println("Gracias por utilizar el programa :)");
         input1.close();
-        //System.out.println("Su complejo es: "+miComplejo.toString());
     }
 }
